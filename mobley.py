6ea5fec919d63fb30a31999fb7e01f54eac64fcb
1Michael23 <michael.laginestra0@gmail.com>
2020-10-30 13:50:05 +1100

    Added settings json

--- mobley.py
+++ mobley.py
@@ -1,3 +1,13 @@
 #mobley, the discord music bot from the vera team
 
+import json
 import discord
+
+# load the settings
+
+f = open("settings.json", "r")
+settings = json.load(f)
+token = settings['token']
+webhook_url = settings['webhook']
+status = settings['status']
+prefix = settings['prefix']
