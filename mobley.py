cf216042f7cf4108901b09bf769ce2f6ba74866c
1Michael23 <michael.laginestra0@gmail.com>
2020-11-02 11:21:16 +1100

    Added some minor discord bits

--- mobley.py
+++ mobley.py
@@ -10,3 +10,26 @@ settings = json.load(f)
 token = settings['token']
 status = settings['status']
 prefix = settings['prefix']
+
+print('trying to run on token "'+token+'"')
+
+# the discord shit
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+
+
+client.run(token)
